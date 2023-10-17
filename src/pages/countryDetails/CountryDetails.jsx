b2ce384ca312438f8912e2dd7c64b0ccf7bc42a5
olawale-oloye <olawaleoloye@gmail.com>
2023-10-17 12:02:16 +1000

    loader for individual country

--- src/pages/countryDetails/CountryDetails.jsx
+++ src/pages/countryDetails/CountryDetails.jsx
@@ -1,17 +1,29 @@
 import { useParams, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
+import giphy from "../../assets/loading.gif";
+// import { DiApple } from "react-icons/di";
 
 const CountryDetails = () => {
   const { name } = useParams();
   const [aCountry, setACountry] = useState();
+  const [loading, setLoading] = useState(false);
+  const [errorTxt, setErrorTxt] = useState(" ");
   const nav = useNavigate();
 
   const getCountryDetails = async () => {
+    setLoading(true);
     const response = await fetch(`https://restcountries.com/v3.1/name/${name}`);
     const data = await response.json();
 
     setACountry(data);
     console.log(data);
+    if (response) setLoading(false);
+    if (response.ok) {
+      setACountry(data);
+    }
+    if (!response.ok) {
+      setErrorTxt("Failed to load");
+    }
   };
 
   useEffect(() => {
@@ -21,12 +33,22 @@ const CountryDetails = () => {
   return (
     <div className="container m-auto">
       <div>
+        {loading && (
+          <img
+            className=" fixed top-[50%] left-[50%] translate-x-[-50%]  translate-y-[-50%]"
+            src={giphy}
+            alt=""
+          />
+        )}
+        {errorTxt && <p> {errorTxt} </p>}
+
         <div className="w-full m-12 p-6 ">
           <button
             onClick={() => nav(`/`)}
             className=" font-semibold pt-2 pb-2 pl-6 pr-6 rounded-lg cursor-pointer border-4"
           >
             <i className="ri-arrow-left-line pr-2"></i>
+            {/* <DiApple /> */}
             Back
           </button>
         </div>
